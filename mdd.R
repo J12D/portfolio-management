49c0070e3f14a861bd175092395802affe9f72d9
ncly <nicolay.schmitt@gmail.com>
2015-10-12 23:06:37 +0200

    change of mdd

--- mdd.R
+++ mdd.R
@@ -1,20 +1,30 @@
 calcMDD <- function(asset) {
   asset <- rev(asset)
-  if (length(asset) < 2) return(0)
-  maxDiffPortVal <- as.numeric(asset[2]) - as.numeric(asset[1])
-  minPortVal <- as.numeric(asset[1])
-  for (i in 1:(length(asset))) {
-    if (as.numeric(asset[i]) - minPortVal > maxDiffPortVal)
-    {
-      maxDiffPortVal <- as.numeric(asset[i]) - minPortVal
-    }
-    if (as.numeric(asset[i]) < minPortVal)
-    {
-      minPortVal <- as.numeric(asset[i])
+  
+  if (as.numeric(asset[2]) - as.numeric(asset[1]) > 0) {
+    maxDiffPortVal <- as.numeric(asset[2]) - as.numeric(asset[1])
+    minPortVal <- as.numeric(asset[1])
+  }
+  else{
+    maxDiffPortVal <- 0
+    minPortVal <- as.numeric(asset[1])
+  }
+  
+  for (i in 1:(length(asset) - 1)) {
+    for (j in (i + 1):(length(asset))) {
+      if (maxDiffPortVal < as.numeric(asset[j]) - as.numeric(asset[i])) {
+        maxDiffPortVal <- as.numeric(asset[j]) - as.numeric(asset[i])
+        minPortVal <- as.numeric(asset[i])
+      }
     }
   }
   mdd <- max(0, (minPortVal + maxDiffPortVal) / minPortVal - 1) * 100
   mdd
 }
 
-assets["2012-01-31/"] %>% rowSums.xts %>% apply.monthly(calcMDD) %>% mean
+assets["2012-12-31/"][,"DAX"] %>%
+  (function(x)
+    x[endpoints(x),]) %>%
+  rollapply(12, calcMDD) %>% na.omit %>%
+  (function(x)
+    sum(x) / (length(x) - 13))
