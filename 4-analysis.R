3e50e86275ad87818400a94fe562fa5a875efc6c
ncly <nicolay.schmitt@gmail.com>
2015-11-04 21:33:12 +0100

    Black Litterman calc

--- 4-analysis.R
+++ 4-analysis.R
@@ -4,8 +4,14 @@ source("0-helper.R")
 source("1-data.R")
 
 
-q <- t(matrix(c(1,0,0,-1, 0,0,1,0, 0,1,0,0, 0,0,0,1), nrow = 4, ncol = 4))
-Pviews <- t(c(0.1,0.02,0.03,0.3))
+P <- t(matrix(c(1,0,0,-1, 0,0,1,0, 0,1,0,0, 0,0,0,1), nrow = 4, ncol = 4))
+v <- matrix(c(0.1,0.02,0.03,0.3))
+Omega <- matrix(c(0.6,0,0,0, 0,0.6,0,0, 0,0,0.6,0, 0,0,0,0.6), nrow = 4, ncol = 4)
 
 
-black.litterman(returns, q, Mu=NULL, Sigma =NULL, Views = Pviews)
+#black.litterman(returns, q, Mu=NULL, Sigma =NULL, Views = Pviews)
+
+Mu <- matrix(mean_returns()(returns))
+Sigma <- matrix(cov_returns()(returns), nrow = 4, ncol = 4)
+
+black.litterman(returns, P, Mu, Sigma, v)
